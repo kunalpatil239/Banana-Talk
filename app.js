1e8d9b2c42c570c5ab64dc2cd0cbe53a9f3bf5fc
Kunal <47500407+kunalpatil239@users.noreply.github.com>
2020-12-13 17:36:04 +0530

    fix: added fetch api

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 var btnTranslate = document.querySelector('#btn-translate');  //now it has reference to the button
 
 var textInput = document.querySelector('#textInput');
+var output = document.querySelector("#output");
 
-btnTranslate.addEventListener('click',function clickHandler(){
-    console.log(textInput.value)
-})
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+     return serverURL + "?" + "text=" + text
+}
+function errorHandler(error){
+    console.lof("error occured");
+}
+
+function clickHandler(){
+    var inputText = textInput.value
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        translatedText = json.contents.translated;
+        output.innerText = translatedText
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener('click', clickHandler);
 
 
